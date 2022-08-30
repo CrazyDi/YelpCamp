bd4725e6dcbb1bffe9151a96ca2fda5c02f6e5aa
Kate Che <43439123+CrazyDi@users.noreply.github.com>
2022-08-30 18:40:01 +0200

    Version 0.0.2 - restyling

--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,13 @@ const seedDB = async () => {
     await Campground.deleteMany({})
     for(let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000)
+        const price = Math.floor(Math.random() * 20) + 10
         const camp = new Campground({
             location: `${sample(cities).city}, ${sample(cities).state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: `https://source.unsplash.com/random/300x300?camping,${i}`,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras interdum ligula non felis sodales malesuada. Pellentesque libero nibh, porta eu erat eget, iaculis convallis neque. Sed blandit sit amet nunc vel accumsan. Praesent sit amet auctor ex, a posuere nibh. Cras est odio, consectetur sit amet pellentesque a, suscipit in dolor.',
+            price
         })
         await camp.save()
     }
